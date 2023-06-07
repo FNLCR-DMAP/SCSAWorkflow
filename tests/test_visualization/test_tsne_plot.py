d0499713a6809fcee94d20b9fd03eeeefc9a0bc5
fangliu117 <107778735+fangliu117@users.noreply.github.com>
2023-06-07 09:23:46 -0400

    refactor: updated test_tsne_plot

--- tests/test_visualization/test_tsne_plot.py
+++ tests/test_visualization/test_tsne_plot.py
@@ -1,7 +1,6 @@
 import unittest
 import anndata
 import numpy as np
-from spac.transformations import tsne
 from spac.visualization import tsne_plot
 
 
@@ -12,34 +11,18 @@ class TestTsnePlot(unittest.TestCase):
         self.adata.obsm['X_tsne'] = np.random.rand(10, 2)
 
     def test_invalid_input_type(self):
-        adata = 10  # Simulate adata being an integer
-
-        # Check if it raises a ValueError with the expected message
         with self.assertRaises(ValueError) as cm:
-            if not isinstance(adata, anndata.AnnData):
-                raise ValueError("adata must be an AnnData object.")
-
-        self.assertEqual(
-            str(cm.exception),
-            "adata must be an AnnData object."
-        )
+            tsne_plot(1)
+        self.assertEqual(str(cm.exception),
+                         "adata must be an AnnData object.")
 
     def test_no_tsne_data(self):
-        # Simulate removal of 'X_tsne' in adata.obsm
         del self.adata.obsm['X_tsne']
-
         with self.assertRaises(ValueError) as cm:
-            if 'X_tsne' not in self.adata.obsm:
-                raise ValueError(
-                    "adata.obsm does not contain 'X_tsne', "
-                    "perform t-SNE transformation first."
-                )
-
-        self.assertEqual(
-            str(cm.exception),
-            "adata.obsm does not contain 'X_tsne', "
-            "perform t-SNE transformation first."
-        )
+            tsne_plot(self.adata)
+        self.assertEqual(str(cm.exception),
+                         "adata.obsm does not contain 'X_tsne',"
+                         " perform t-SNE transformation first.")
 
 
 if __name__ == '__main__':
