ebc251fcafecc9e55512590288d9cf361d4ae506
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-08-03 15:51:59 -0400

    feat(setup.py): Create script.

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spac',
+    version='0.2.0',
+    description=(
+        'SPatial Analysis for single-Cell analysis (SPAC)'
+        'is a Scalable Python package for single-cell spatial protein data '
+        'analysis from multiplexed whole-slide tissue images.'
+    ),
+    long_description=(
+        'SPAC is a scalable, Python-based package for analyzing single-cell '
+        'spatial protein data from multiplexed whole-slide tissue images. It '
+        'integrates with other single-cell toolkits through the anndata '
+        'framework and provides various functional and visualization modules. '
+        'SPAC enables user-friendly web interfaces and offers insights into '
+        'cell interactions in diverse environments, benefiting studies of '
+        'cancer microenvironments, stem cell niches, and drug responses.'
+    ),
+    author='Fang Liu, Rui He, and George Zaki',
+    url='https://github.com/FNLCR-DMAP/SCSAWorkflow',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'anndata',
+        'numpy',
+        'scikit-learn',
+        'squidpy',
+        'matplotlib',
+        'seaborn',
+        'scanpy',
+        'phenograph==1.5.7',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD 3-Clause License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
