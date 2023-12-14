d3bcf35a7091660872b52a5d279d384349c5e55f
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-12-14 15:03:18 -0500

    docs: update __init__.py

--- src/spac/__init__.py
+++ src/spac/__init__.py
@@ -1,1 +1,27 @@
-__version__=0.1
+# spac/__init__.py
+
+import os
+import glob
+
+
+# Define the subdirectory where you want to find Python modules
+subdirectory = "src"
+
+# Get a list of all Python module files in the package directory
+module_files = glob.glob(os.path.join(os.path.dirname(__file__), subdirectory, "*.py"))
+
+# Exclude the "__init__.py" file itself from the list
+module_files = [f for f in module_files if not f.endswith("__init__.py")]
+
+# Dynamically import all functions from all modules
+for module_file in module_files:
+    module_name = os.path.basename(module_file)[:-3]  # Remove the ".py" extension
+    module = __import__(f"{__name__}.{module_name}", fromlist=["*"])
+    functions = [name for name in dir(module) if callable(getattr(module, name))]
+    globals().update({name: getattr(module, name) for name in functions})
+
+# Define the package version before using it in __all__
+__version__ = "0.3.1"
+
+# Define a __all__ list to specify which functions should be considered public
+__all__ = functions
