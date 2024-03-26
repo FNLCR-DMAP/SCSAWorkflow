50410c22414b833fd69b76a7da23bc179d3ee1da
fangliu117 <107778735+fangliu117@users.noreply.github.com>
2024-03-26 12:24:05 -0400

    refactor(select_values): remove unnecessary logging and add astype(str)

--- src/spac/data_utils.py
+++ src/spac/data_utils.py
@@ -527,29 +527,24 @@ def select_values(data, annotation, values=None):
     # Validate provided values against unique ones, if not None
     if values is not None:
         if isinstance(data, pd.DataFrame):
-            unique_values = data[annotation].unique().tolist()
+            unique_values = data[annotation].astype(str).unique().tolist()
         elif isinstance(data, ad.AnnData):
-            unique_values = data.obs[annotation].unique().tolist()
+            unique_values = data.obs[annotation].astype(str).unique().tolist()
         check_list_in_list(
             values, "values", "label", unique_values, need_exist=True
         )
 
-    # Proceed with filtering based on data type
+    # Proceed with filtering based on data type and count matching cells
     if isinstance(data, pd.DataFrame):
         filtered_data = data if values is None else \
             data[data[annotation].isin(values)]
+        count = filtered_data.shape[0]
     elif isinstance(data, ad.AnnData):
         filtered_data = data if values is None else \
             data[data.obs[annotation].isin(values)]
-
-    # Summary information
-    if isinstance(filtered_data, pd.DataFrame):
-        count = filtered_data.shape[0]
-    elif isinstance(filtered_data, ad.AnnData):
         count = filtered_data.n_obs
-        logging.info("Note: The summary pertains to the default layer "
-                     "of the AnnData object.")
-    logging.info(f"Summary of returned dataset: {count} rows (cells) "
+
+    logging.info(f"Summary of returned dataset: {count} cells "
                  "match the selected labels.")
 
     return filtered_data
