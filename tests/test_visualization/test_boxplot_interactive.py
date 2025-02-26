cc1e7adc482cd8c46e9b2cf858aefe95612c9d14
Ahmad Abdallah <abmahmoud03@gmail.com>
2025-02-26 10:08:59 -0500

    revert boxplot tests, add boxplot_interactive tests

--- tests/test_visualization/test_boxplot_interactive.py
+++ tests/test_visualization/test_boxplot_interactive.py
@@ -3,13 +3,11 @@ from unittest import mock
 import pandas as pd
 import anndata
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-from spac.visualization import boxplot
-matplotlib.use('Agg')  # Set the backend to 'Agg' to suppress plot window
+import plotly.graph_objects as go
+from spac.visualization import boxplot_interactive
 
 
-class TestBoxplot(unittest.TestCase):
+class TestBoxplotInteractive(unittest.TestCase):
 
     def setUp(self):
         """Set up testing environment."""
@@ -31,75 +29,84 @@ class TestBoxplot(unittest.TestCase):
 
     def test_returns_correct_types(self):
         """Test if correct types are returned."""
-        fig, ax, df = boxplot(self.adata)
-        self.assertIsInstance(fig, plt.Figure)
-        self.assertIsInstance(ax, plt.Axes)
+        # Test non-interactive mode
+        fig, df, metrics = boxplot_interactive(self.adata, interactive=False, return_metrics=True)
+        self.assertIsInstance(fig, str)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertIsInstance(metrics, pd.DataFrame)
 
-    def test_second_annotation_mode(self):
-        """Test if second annotation mode works as expected."""
-        self.adata.obs['treatment'] = ['Drug1', 'Drug1', 'Drug2', 'Drug2']
-        fig, ax, df = boxplot(
-            self.adata,
-            annotation='phenotype',
-            second_annotation='treatment',
-            orient='v'
-        )
-        xtick_labels = [tick.get_text() for tick in ax.get_xticklabels()]
-        for label in ['phenotype1', 'phenotype2']:
-            self.assertIn(label, xtick_labels)
-        legend_labels = [text.get_text() for text in ax.legend().get_texts()]
-        for label in ['Drug1', 'Drug2']:
-            self.assertIn(label, legend_labels)
+        # Test interactive mode
+        fig, df = boxplot_interactive(self.adata, interactive=True)
+        self.assertIsInstance(fig, go.Figure)
+        self.assertIsInstance(df, pd.DataFrame)
+
 
     def test_single_annotation_single_feature(self):
         """Test if annotation mode works as expected with a single feature."""
-        fig, ax, df = boxplot(
+        fig, df = boxplot_interactive(
             self.adata, features=['feature1'],
-            annotation='phenotype', orient='v'
+            annotation='phenotype', 
+            orient='v',
+            interactive=True
         )
-        xtick_labels = [tick.get_text() for tick in ax.get_xticklabels()]
+
+        # Get boxplot names from figure
+        box_name_data = [box.name for box in fig.data]
+
+        # Check if box names match the expected phenotype annotations
         expected_unique_labels = ['phenotype1', 'phenotype2']
         for label in expected_unique_labels:
-            self.assertIn(label, xtick_labels)
+            self.assertIn(label, box_name_data)
+
 
     def test_single_annotation_multiple_features(self):
         """Test when only one annotation but multiple features."""
-        fig, ax, df = boxplot(
+        fig, df = boxplot_interactive(
             self.adata,
             features=['feature1', 'feature2'],
-            annotation='phenotype', orient='v'
+            annotation='phenotype', 
+            orient='v',
+            interactive=True,
         )
-        xtick_labels = [tick.get_text() for tick in ax.get_xticklabels()]
-        expected_unique_labels = ['feature1', 'feature2']
-        for label in expected_unique_labels:
-            self.assertIn(label, xtick_labels)
+
+        # Get x-axis labels from figure
+        x_labels = fig.data[0].x
+
+        # Check if x-axis labels match the feature names
+        expected_features = ['feature1', 'feature2']
+        self.assertEqual(set(x_labels), set(expected_features))
+
 
     def test_multiple_features_mode(self):
         """Test if multiple features mode works as expected."""
-        fig, ax, df = boxplot(
+        fig, df = boxplot_interactive(
             self.adata,
             features=['feature1', 'feature2'],
-            orient='v'
+            orient='v',
+            interactive=True,
         )
 
-        # Get x-tick labels from the plot
-        xtick_labels = [tick.get_text() for tick in ax.get_xticklabels()]
+        #  Get x-axis labels from figure
+        x_labels = [x_data for boxplot in fig.data for x_data in boxplot.x]
 
-        # Check if x-tick labels match the feature names
+        # Check if x-axis labels match the feature names
         expected_features = ['feature1', 'feature2']
-        self.assertEqual(set(xtick_labels), set(expected_features))
+        self.assertEqual(set(x_labels), set(expected_features))
 
-    def test_orient_kwarg(self):
-        """Test for the orient parameter passed via **kwargs."""
-        fig, ax, df = boxplot(
+    def test_orient(self):
+        """Test for the orient parameter."""
+        fig, df = boxplot_interactive(
             self.adata, features=['feature1'],
-            annotation='phenotype', orient='h'
+            annotation='phenotype', 
+            orient='h',
+            interactive=True,
         )
-        # Check if the x-axis ticks contain the expected annotations
-        ytick_labels = [tick.get_text() for tick in ax.get_yticklabels()]
-        expected_unique_labels = ['phenotype1', 'phenotype2']
-        for label in expected_unique_labels:
-            self.assertIn(label, ytick_labels)
+        # Get y-axis labels from figure
+        y_labels = [y_data for boxplot in fig.data for y_data in boxplot.y]
+
+        # Check if y-axis labels match the feature names (meaning orientation is correct)
+        expected_features = ['feature1']
+        self.assertEqual(set(y_labels), set(expected_features))
 
     def test_log_scale(self):
         """Test for the log_scale parameter."""
@@ -108,21 +115,21 @@ class TestBoxplot(unittest.TestCase):
             [0.693147, 1.386294, 1.791759, 2.079441], dtype=np.float64
         )
 
-        fig, ax, df = boxplot(
-            self.adata, features=['feature1'], log_scale=True
+        fig, df = boxplot_interactive(
+            self.adata, 
+            features=['feature1'],
+            log_scale=True,
+            interactive=True
         )
 
         # Check that the y-axis label is 'log(Intensity)'
-        self.assertEqual(ax.get_ylabel(), 'log(Intensity)')
+        self.assertEqual(fig.layout.yaxis.title.text, 'log(Intensity)')
 
         # Check that the data has been transformed via np.log1p
         transformed_values = df['feature1'].values
         np.testing.assert_allclose(
             transformed_values, expected_values, rtol=1e-5
         )
-
-        # Check that the y-axis scale is 'linear'
-        self.assertEqual(ax.get_yscale(), 'linear')
 
         # Test with zero values
         self.adata.X[0, 0] = 0  # Introduce a zero value
@@ -132,21 +139,21 @@ class TestBoxplot(unittest.TestCase):
             [0.0, 1.386294, 1.791759, 2.079441], dtype=np.float64
         )
 
-        fig, ax, df = boxplot(
-            self.adata, features=['feature1'], log_scale=True
+        fig, df = boxplot_interactive(
+            self.adata, 
+            features=['feature1'], 
+            log_scale=True,
+            interactive=True
         )
 
         # Check that the y-axis label is still 'log(Intensity)'
-        self.assertEqual(ax.get_ylabel(), 'log(Intensity)')
+        self.assertEqual(fig.layout.yaxis.title.text, 'log(Intensity)')
 
         # Check that the data has been transformed via np.log1p
         transformed_values = df['feature1'].values
         np.testing.assert_allclose(
             transformed_values, expected_values_zero, rtol=1e-5
         )
-
-        # The y-axis scale should still be 'linear'
-        self.assertEqual(ax.get_yscale(), 'linear')
 
     def test_log1p_transformation(self):
         """Test if np.log1p transformation is applied correctly."""
@@ -172,12 +179,12 @@ class TestBoxplot(unittest.TestCase):
         )
 
         # Create a boxplot and capture the transformed DataFrame
-        fig, ax, transformed_df = boxplot(
+        fig, df = boxplot_interactive(
             adata, features=['feature1'], log_scale=True
         )
 
         # Extract the log1p transformed values from the DataFrame for plotting
-        transformed_values = transformed_df['feature1'].values.flatten()
+        transformed_values = df['feature1'].values.flatten()
 
         # Debugging: print DataFrame after transformation
         print("DataFrame after log1p transformation:\n", transformed_values)
@@ -207,7 +214,12 @@ class TestBoxplot(unittest.TestCase):
         adata = anndata.AnnData(X=X.astype(np.float32), obs=annotation)
 
         # Create a boxplot and capture the print output
-        fig, ax, df = boxplot(adata, features=['feature1'], log_scale=True)
+        fig, df = boxplot_interactive(
+            adata, 
+            features=['feature1'], 
+            log_scale=True,
+            interactive=True
+        )
 
         # Extract the printed messages
         print_calls = [call.args[0] for call in mock_print.call_args_list]
@@ -220,7 +232,7 @@ class TestBoxplot(unittest.TestCase):
         self.assertIn(expected_msg, print_calls)
 
         # Ensure the y-axis label is 'Intensity'
-        self.assertEqual(ax.get_ylabel(), 'Intensity')
+        self.assertEqual(fig.layout.yaxis.title.text, 'Intensity')
 
         # Expected values (should be the same as input
         # since loig_scale is disabled)
@@ -238,66 +250,85 @@ class TestBoxplot(unittest.TestCase):
         based on orientation.
         """
         # Test for vertical orientation
-        fig, ax, df = boxplot(self.adata, features=['feature1'], orient='v')
-        self.assertEqual(ax.get_ylabel(), 'Intensity')
+        fig, df = boxplot_interactive(
+            self.adata, 
+            features=['feature1'], 
+            orient='v', 
+            interactive=True
+        )
+        self.assertEqual(fig.layout.yaxis.title.text, 'Intensity')
 
         # Test for horizontal orientation
-        fig, ax, df = boxplot(self.adata, features=['feature1'], orient='h')
-        self.assertEqual(ax.get_xlabel(), 'Intensity')
+        fig, df = boxplot_interactive(self.adata, features=['feature1'], orient='h', interactive=True)
+        self.assertEqual(fig.layout.xaxis.title.text, 'Intensity')
 
     def test_axis_labels(self):
         """Test x-axis, y-axis, and log labeling."""
         # Test for vertical orientation without log scale
-        fig, ax, df = boxplot(
-            self.adata,
-            features=['feature1'],
-            annotation='phenotype',
-            orient='v'
-        )
-        self.assertEqual(ax.get_xlabel(), 'phenotype')
-        self.assertEqual(ax.get_ylabel(), 'Intensity')
-
-        # Test for vertical orientation with log scale
-        fig, ax, df = boxplot(
+        fig, df = boxplot_interactive(
             self.adata,
             features=['feature1'],
             annotation='phenotype',
             orient='v',
-            log_scale=True
+            interactive=True
         )
-        self.assertEqual(ax.get_xlabel(), 'phenotype')
-        self.assertEqual(ax.get_ylabel(), 'log(Intensity)')
+        self.assertEqual(fig.layout.xaxis.title.text, 'phenotype')
+        self.assertEqual(fig.layout.yaxis.title.text, 'Intensity')
 
-        # Test for horizontal orientation without log scale
-        fig, ax, df = boxplot(
+        # Test for vertical orientation with log scale
+        fig, df = boxplot_interactive(
             self.adata,
             features=['feature1'],
             annotation='phenotype',
-            orient='h'
+            orient='v',
+            log_scale=True,
+            interactive=True
         )
-        self.assertEqual(ax.get_xlabel(), 'Intensity')
-        self.assertEqual(ax.get_ylabel(), 'phenotype')
+        self.assertEqual(fig.layout.xaxis.title.text, 'phenotype')
+        self.assertEqual(fig.layout.yaxis.title.text, 'log(Intensity)')
 
-        # Test for horizontal orientation with log scale
-        fig, ax, df = boxplot(
+        # Test for horizontal orientation without log scale
+        fig, df = boxplot_interactive(
             self.adata,
             features=['feature1'],
             annotation='phenotype',
             orient='h',
-            log_scale=True
+            interactive=True
         )
-        self.assertEqual(ax.get_xlabel(), 'log(Intensity)')
-        self.assertEqual(ax.get_ylabel(), 'phenotype')
+        self.assertEqual(fig.layout.xaxis.title.text, 'Intensity')
+        self.assertEqual(fig.layout.yaxis.title.text, 'phenotype')
+
+        # Test for horizontal orientation with log scale
+        fig, df = boxplot_interactive(
+            self.adata,
+            features=['feature1'],
+            annotation='phenotype',
+            orient='h',
+            log_scale=True,
+            interactive=True
+        )
+        self.assertEqual(fig.layout.xaxis.title.text, 'log(Intensity)')
+        self.assertEqual(fig.layout.yaxis.title.text, 'phenotype')
 
     def test_single_feature_labeling(self):
         """Test if single feature name is displayed correctly on the x-axis."""
-        fig, ax, df = boxplot(self.adata, features=['feature1'], orient='v')
-        xtick_labels = [tick.get_text() for tick in ax.get_xticklabels()]
-        self.assertEqual(xtick_labels, ['feature1'])
+        fig, df = boxplot_interactive(
+            self.adata, 
+            features=['feature1'], 
+            orient='v',
+            interactive=True,
+        )
+        x_labels = [x_data for boxplot in fig.data for x_data in boxplot.x]
+        self.assertEqual(x_labels, ['feature1'])
 
-        fig, ax, df = boxplot(self.adata, features=['feature1'], orient='h')
-        ytick_labels = [tick.get_text() for tick in ax.get_yticklabels()]
-        self.assertEqual(ytick_labels, ['feature1'])
+        fig, df = boxplot_interactive(
+            self.adata, 
+            features=['feature1'], 
+            orient='h',
+            interactive=True,
+        )
+        y_labels = [y_data for boxplot in fig.data for y_data in boxplot.y]
+        self.assertEqual(y_labels, ['feature1'])
 
 
 if __name__ == '__main__':
