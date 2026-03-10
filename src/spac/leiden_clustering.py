a3cdb4cd69e70541841d564f37a0633bdd6d5e10
ramyap06 <prasannr@purdue.edu>
2026-03-10 14:37:54 -0400

    MVP of leiden clustering

---
+++ src/spac/leiden_clustering.py
@@ -0,0 +1,29 @@
+import scipy
+import squidpy as sq
+import scanpy as sc
+import pandas as pd
+import numpy as np
+import typing as tp
+import parmap
+import anndata
+from tqdm import tqdm
+
+# leiden docs - https://scanpy.readthedocs.io/en/stable/generated/scanpy.tl.leiden.html
+# anndata docs - https://anndata.readthedocs.io/en/latest/tutorials/notebooks/getting-started.html
+
+# read the docs and make a comment about what sc.tl.leiden is doing
+
+def preprocess(adata):
+    ad = adata.copy()
+    sc.tl.pca(ad)
+    sc.pp.neighbors(ad)
+    sc.tl.umap(ad)
+    return ad
+
+def leiden_only_clustering(adata):
+    ad = adata.copy()
+    sc.tl.leiden(ad, key_added="leiden_clusters")
+    return ad
+
+def plot(adata):
+    sc.pl.umap(adata, color="leiden_clusters")
